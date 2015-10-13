fcd7f7e2d02787d39e841ccd8fa565fb85f98787
Uzair Sharif <uzair_sharif@live.com>
2015-10-13 13:56:32 +0200

    system now working .. currently all requests are just being forwarded without knowing which cpu issued them

--- projects_repo/eclipse_workspace/ArmVersatileExpress/cache_controller.cpp
+++ projects_repo/eclipse_workspace/ArmVersatileExpress/cache_controller.cpp
@@ -26,21 +26,11 @@ cache_controller::cache_controller(sc_core::sc_module_name name, int num_smp_cor
 void cache_controller::b_transport(int SocketId, tlm::tlm_generic_payload &payload, sc_core::sc_time &delay) {
 	if (SocketId<4) {			// request from INSTRUCTION BUS
 		assert(SocketId == 0);
-
-		if (payload.get_command() == tlm::TLM_READ_COMMAND) {
-			printf("(CC..IBUS..ACTUAL) addr:0x%08x\r\n", payload.get_address());
-		}
-
 		m_Ibus_isocket->b_transport(payload, delay);
 	}
 
-	if (SocketId>4 && SocketId<8) {		// request from DATA BUS
+	if (SocketId>=4 && SocketId<8) {		// request from DATA BUS
 		assert(SocketId == 4);
-
-		if (payload.get_command() == tlm::TLM_READ_COMMAND) {
-			printf("(CC..DBUS..ACTUAL) addr:0x%08x\r\n", payload.get_address());
-		}
-
 		m_Dbus_isocket->b_transport(payload, delay);
 	}
 }
@@ -49,28 +39,11 @@ void cache_controller::b_transport(int SocketId, tlm::tlm_generic_payload &paylo
 unsigned int cache_controller::transport_dbg(int SocketId, tlm::tlm_generic_payload &payload) {
 	if (SocketId<4) {			// request from INSTRUCTION BUS
 		assert(SocketId == 0);
-
-		static int count = 0;
-		if (SocketId == 0 && payload.get_command() == tlm::TLM_READ_COMMAND) {
-			printf("(CC..IBUS..DEBUG) addr:0x%08x\r\n", payload.get_address());
-			count++;
-			if (count == 100) {
-				assert(0);
-			}
-		}
-
-
 		return m_Ibus_isocket->transport_dbg(payload);
 	}
 
 	if (SocketId>=4 && SocketId<8) {		// request from DATA BUS
 		assert(SocketId == 4);
-
-		if (payload.get_command() == tlm::TLM_READ_COMMAND) {
-			printf("(CC..DBUS..DEBUG) addr:0x%08x\r\n", payload.get_address());
-		}
-
-
 		return m_Dbus_isocket->transport_dbg(payload);
 	}
 }
